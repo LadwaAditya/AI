9a631316113f7e0da9eba98f356338fc02bdbebf
Aditya Ladwa <ladwa.aditya@gmail.com>
2018-03-03 20:59:22 +0530

    Showing stock result

--- 01_intro/4_stock.py
+++ 01_intro/4_stock.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 dates = []
 prices = []
 
-plt.switch_backend('new_backend')
+# plt.switch_backend('new_backend')
 
 def get_data(filename):
     with open(filename,'r') as csvfile:
@@ -15,23 +15,23 @@ def get_data(filename):
         next(csvFileReader)
         for row in csvFileReader:
             dates.append(int(row[0].split('-')[0]))
-            prices.append(row[1])
+            prices.append(float(row[1]))
     return
 
-def predict_prices(dates,prices,x):
-    dates = np.reshape(dates,len(dates),1)
+def predict_price(dates,prices,x):
+    dates = np.reshape(dates,(len(dates), 1))
+    
+    svr_lin = SVR(kernel='linear', C=1e3)
+    svr_poly = SVR(kernel='poly', C=1e3, degree=2)
+    svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
 
-    svr_len = SVR(kernel='linear',C=1e3)
-    svr_poly = SVR(kernel='poly',C=1e3,degree=2)
-    svr_rbf = SVR(kernel='rbf',C=1e3,gamma=0.1)
-
-    svr_len.fit(dates,prices)
+    svr_lin.fit(dates,prices)
     svr_poly.fit(dates,prices)
     svr_rbf.fit(dates,prices)
 
     plt.scatter(dates,prices,color='black',label='Data')
     plt.plot(dates,svr_rbf.predict(dates),color='red',label='RBF model')
-    plt.plot(dates,svr_len.predict(dates),color='green',label='Linear model')
+    plt.plot(dates,svr_lin.predict(dates),color='green',label='Linear model')
     plt.plot(dates,svr_poly.predict(dates),color='blue',label='Polynomial model')
     plt.xlabel('Date')
     plt.ylabel('Prices')
@@ -39,10 +39,10 @@ def predict_prices(dates,prices,x):
     plt.legend()
     plt.show()
 
-    return svr_rbf.predict(x)[0],svr_len.predict(x)[0],svr_poly.predict(x)[0]
+    return svr_rbf.predict(x)[0],svr_lin.predict(x)[0],svr_poly.predict(x)[0]
 
 get_data('aapl.csv')
 
-predicted_price = predict_prices(dates,predict_prices,29)
+predicted_price = predict_price(dates,prices,29)
 
 print(predicted_price)
